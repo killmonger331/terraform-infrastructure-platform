57489fd1783d29e2ae35fcdd9bf15979ea3bc18e
killmonger331 <richardmorales_24@hotmail.com>
2026-08-15 17:51:16 -0700

    bug fixes

--- terraform/environments/dev/network.tf
+++ terraform/environments/dev/network.tf
@@ -10,129 +10,136 @@
 # Private DB:  10.0.22.0/24
 
 
-#---------------------
-# AZs
-#---------------------
+# ---------------------------------------------------------
+# Availability Zones
+# ---------------------------------------------------------
 
 data "aws_availability_zones" "available" {
   state = "available"
 }
-#---------------------
+
+
+# ---------------------------------------------------------
 # Public Subnets
-#---------------------
+# ---------------------------------------------------------
+
 resource "aws_subnet" "public_a" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = data.aws_availability_zones.available.names[0]
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[0]
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "terraform-platform-dev-public-a"
-    Enviornment = "dev"
-    Type = "public"
-    ManagedBy = "Terraform"
+    Name        = "terraform-platform-dev-public-a"
+    Environment = "dev"
+    Type        = "public"
+    ManagedBy   = "Terraform"
   }
 }
 
 resource "aws_subnet" "public_b" {
-    vpc_id = aws_vpc.main.id
-    cidr_block = "10.0.2.0/24"
-    availablility_zone = data.aws_availability_zones.available.names[1]
-    map_public_ip_on_launch = true
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[1]
+  map_public_ip_on_launch = true
 
-    tags = {
-      Name = "terraform-platform-dev-public-b"
-      Environment = "dev"
-      Type = "public"
-      ManagedBy = "Terraform"
-    }
+  tags = {
+    Name        = "terraform-platform-dev-public-b"
+    Environment = "dev"
+    Type        = "public"
+    ManagedBy   = "Terraform"
+  }
 }
 
-#---------------------
+
+# ---------------------------------------------------------
 # Private Application Subnets
-#---------------------
+# ---------------------------------------------------------
 
 resource "aws_subnet" "private_app_a" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.11.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.11.0/24"
   availability_zone = data.aws_availability_zones.available.names[0]
 
   tags = {
-    Name = "terraform-platform-dev-private-app-a"
+    Name        = "terraform-platform-dev-private-app-a"
     Environment = "dev"
-    Type = "private-app"
-    ManagedBy = "Terraform"
+    Type        = "private-app"
+    ManagedBy   = "Terraform"
   }
 }
 
 resource "aws_subnet" "private_app_b" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.12./24"
-  availability_zone = data.aws_availaability_zones.available.names[1]
-  
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.12.0/24"
+  availability_zone = data.aws_availability_zones.available.names[1]
+
   tags = {
-    Name = "terraform-platform-dev-private-app-b"
+    Name        = "terraform-platform-dev-private-app-b"
     Environment = "dev"
-    Type = "private-app"
-    ManagedBy = "Terraform"
+    Type        = "private-app"
+    ManagedBy   = "Terraform"
   }
 }
 
-#---------------------
-# Private Database Subnets
-#---------------------
 
-resource "aws_subnet" :private_db_a" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.21.0/24"
+# ---------------------------------------------------------
+# Private Database Subnets
+# ---------------------------------------------------------
+
+resource "aws_subnet" "private_db_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.21.0/24"
   availability_zone = data.aws_availability_zones.available.names[0]
-  
+
   tags = {
-    Name = "terraform-platform-dev-private-db-a"
-    Enviornment = "dev"
-    Type = "private_db"
-    ManagedBy = "Terraform"
+    Name        = "terraform-platform-dev-private-db-a"
+    Environment = "dev"
+    Type        = "private-db"
+    ManagedBy   = "Terraform"
   }
 }
 
 resource "aws_subnet" "private_db_b" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.22.0/24
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.22.0/24"
   availability_zone = data.aws_availability_zones.available.names[1]
 
   tags = {
-    Name = "terraform-platform-dev-private-db-b"
+    Name        = "terraform-platform-dev-private-db-b"
     Environment = "dev"
-    Type = "private-db"
-    ManagedBy = "Terraform"
+    Type        = "private-db"
+    ManagedBy   = "Terraform"
   }
 }
 
-#---------------------
+
+# ---------------------------------------------------------
 # Internet Gateway
-#---------------------
+# ---------------------------------------------------------
 
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
-  
+
   tags = {
-    Name = "terraform-platform-dev-igw"
+    Name        = "terraform-platform-dev-igw"
     Environment = "dev"
-    ManagedBy = "Terraform"
+    ManagedBy   = "Terraform"
   }
 }
 
-#---------------------
+
+# ---------------------------------------------------------
 # Elastic IPs for NAT Gateways
-#---------------------
+# ---------------------------------------------------------
 
 resource "aws_eip" "nat_a" {
   domain = "vpc"
 
   tags = {
-    Name = "terraform-platform-dev-nat-eip-a"
-    Enviornment = "dev"
-    ManagedBy = "Terraform"
+    Name        = "terraform-platform-dev-nat-eip-a"
+    Environment = "dev"
+    ManagedBy   = "Terraform"
   }
 }
 
@@ -140,64 +147,68 @@ resource "aws_eip" "nat_b" {
   domain = "vpc"
 
   tags = {
-    Name = "terraform-platform-dev-nat-eip-b"
+    Name        = "terraform-platform-dev-nat-eip-b"
     Environment = "dev"
-    ManagedBy = "Terraform"
+    ManagedBy   = "Terraform"
   }
 }
 
-#---------------------
+
+# ---------------------------------------------------------
 # NAT Gateways
-#---------------------
+# ---------------------------------------------------------
 
 resource "aws_nat_gateway" "a" {
   allocation_id = aws_eip.nat_a.id
-  subnet_id = aws_subnet.public_a.id
+  subnet_id     = aws_subnet.public_a.id
 
-  depends_on [aws_internet_gateway.main]
+  depends_on = [aws_internet_gateway.main]
 
   tags = {
-    Name = "terraform-platform-dev-nat-a"
-    Enviornment = "dev"
-    ManagedBy = "Terraform"
+    Name        = "terraform-platform-dev-nat-a"
+    Environment = "dev"
+    ManagedBy   = "Terraform"
   }
 }
 
 resource "aws_nat_gateway" "b" {
   allocation_id = aws_eip.nat_b.id
-  subnet_id = aws_subnet.public_b.id
+  subnet_id     = aws_subnet.public_b.id
 
-  depends_on [aws_internet_gateway.main]
+  depends_on = [aws_internet_gateway.main]
 
   tags = {
-    Name = "terraform-platform-dev-nat-b"
-    Enviornment = "dev"
-    ManagedBy = "Terraform"
+    Name        = "terraform-platform-dev-nat-b"
+    Environment = "dev"
+    ManagedBy   = "Terraform"
   }
 }
 
-#---------------------
+
+# ---------------------------------------------------------
 # Public Route Table
-#---------------------
+# ---------------------------------------------------------
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
   tags = {
-    Name = "terraform-platform-dev-public-rt"
-    Enviornment = "dev"
-    ManagedBy = "Terraform"
+    Name        = "terraform-platform-dev-public-rt"
+    Environment = "dev"
+    ManagedBy   = "Terraform"
   }
 }
+
 resource "aws_route" "public_internet" {
-  route_table_id = aws_route_table.public.id
+  route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.main.id
+  gateway_id             = aws_internet_gateway.main.id
 }
 
-#---------------------
+
+# ---------------------------------------------------------
 # Private Application Route Tables
-#---------------------
+# ---------------------------------------------------------
 
 resource "aws_route_table" "private_app_a" {
   vpc_id = aws_vpc.main.id
@@ -232,9 +243,9 @@ resource "aws_route" "private_app_b_nat" {
 }
 
 
-# ----------------------
+# ---------------------------------------------------------
 # Private Database Route Table
-# ----------------------
+# ---------------------------------------------------------
 
 resource "aws_route_table" "private_db" {
   vpc_id = aws_vpc.main.id
@@ -247,9 +258,9 @@ resource "aws_route_table" "private_db" {
 }
 
 
-# ---------------------
+# ---------------------------------------------------------
 # Route Table Associations
-# ---------------------
+# ---------------------------------------------------------
 
 resource "aws_route_table_association" "public_a" {
   subnet_id      = aws_subnet.public_a.id
